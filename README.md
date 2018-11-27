0aec949b24f77712a57b743931a1ad41b37e78f1
Ramesh Suraparaju <ramesh.suraparaju@sap.com>
2018-11-27 10:52:27 +0800

    working version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nodejs
+This is my first node.js application
--- firstApp/index.js
+++ firstApp/index.js
@@ -1,11 +1,9 @@
 var express = require("express");
-
 var app = express();
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
-app.listen(3000);
 
 app.get("/", function(request, response) {
   response.render("homePage");
@@ -13,4 +11,8 @@ app.get("/", function(request, response) {
 
 app.get("/test", function(request, response) {
   response.render("testPage");
-})
+});
+
+app.listen(3000, function() {
+  console.console.log("firstApp listening on port 3000");
+});
